0aef27ae06b1b92d345d156382e0c26b59bea03c
jracca <jun.racca@optum.com>
2025-04-08 16:54:16 -0700

    Fix bad merge

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ import pygame_gui
 
 
 class EventController:
-    def __init__(self, start: pygame_gui.elements.UIButton, next: pygame_gui.elements.UIButton, reset: pygame_gui.elements.UIButton):
+    def __init__(self, start: pygame_gui.elements.UIButton, next: pygame_gui.elements.UIButton, reset: pygame_gui.elements.UIButton, shapes: pygame_gui.elements.UIDropDownMenu):
         self.start_button = start
         self.next_button = next
         self.reset_button = reset
